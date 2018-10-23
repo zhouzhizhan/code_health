8d8fd4fd88a174791b9816a36b6b7ae6b99c1c1d
Liangdi <wu@liangdi.me>
2018-10-23 13:31:11 +0800

    更改标题

--- code_health/lib/main.dart
+++ code_health/lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Dem',
+      title: 'Flutter Demf',
       theme: ThemeData(
         // This is the theme of your application.
         //
